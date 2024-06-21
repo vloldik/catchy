3a91d1b33a01669247806a133dbc03c40753670d
Vloldik <99754299+vloldik@users.noreply.github.com>
2024-06-21 23:39:08 +0300

    update to v2

--- catchy.go
+++ catchy.go
@@ -1,59 +1,70 @@
 package catchy
 
-func Must[T interface{}](o T, err error) T {
-	if err != nil {
-		panic(err)
-	}
-	return o
-}
-
-func MustNoReturn(err error) {
-	Must(never, err)
-}
-
 type Never = interface{}
 
 var never = Never(nil)
+
+type IDoable interface {
+	Do() error
+}
 
 type Catchy[T interface{}] struct {
 	GetValue func() (T, error)
 	OnSucess func(T)
 	OnError  func(error)
+	next     *DoableNode
+	last     *DoableNode
 }
 
-func WithGetValueFunc[T interface{}](getValue func() (T, error)) Catchy[T] {
-	return Catchy[T]{
-		GetValue: getValue,
-	}
-}
-
-func WithNoReturnFunc(noReturnFunc func() error) Catchy[Never] {
-	return Catchy[Never]{
-		GetValue: func() (Never, error) {
-			return never, noReturnFunc()
-		},
-	}
-}
-
-func (c Catchy[T]) WithOnSuccess(useValue func(T)) Catchy[T] {
+func (c *Catchy[T]) WithOnSuccess(useValue func(T)) *Catchy[T] {
 	c.OnSucess = useValue
 	return c
 }
 
-func (c Catchy[T]) WithOnError(onError func(error)) Catchy[T] {
+func (c *Catchy[T]) WithOnError(onError func(error)) *Catchy[T] {
 	c.OnError = onError
 	return c
 }
 
-func (c Catchy[T]) Do() {
+func (c *Catchy[T]) WithGetValueFunc(getValue func() (T, error)) *Catchy[T] {
+	c.GetValue = getValue
+	return c
+}
+
+func (c Catchy[T]) doSelf() error {
 	value, err := c.GetValue()
 	if err != nil && c.OnError != nil {
 		c.OnError(err)
-		return
+		return err
 	} else if err != nil {
-		panic(err)
+		return err
 	}
 	if c.OnSucess != nil {
 		c.OnSucess(value)
 	}
+	return nil
+}
+
+func (c Catchy[T]) Do() error {
+	err := c.doSelf()
+	if err != nil {
+		return err
+	}
+	if c.next != nil {
+		return c.next.Do()
+	}
+
+	return nil
+}
+
+func (c *Catchy[T]) DoNext(next IDoable) *Catchy[T] {
+	Node := newDoableNode(next)
+	if c.next == nil {
+		c.next = Node
+		c.last = Node
+	} else {
+		c.last.Next = Node
+		c.last = Node
+	}
+	return c
 }
